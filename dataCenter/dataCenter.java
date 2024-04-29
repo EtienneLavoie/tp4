e1cb1ec633defe6c3d5d69ed39e9e3cde7b84e66
https://github.com/filpageau/TP3---F-lix-Pageau-et-Etienne-Lavoie.git <2241940@cegeplimoilou.ca>
2024-04-29 11:12:26 -0400

    création projet

---
+++ dataCenter/dataCenter.java
@@ -0,0 +1,24 @@
+package dataCenter;
+
+public class dataCenter {
+
+    public String get(int colonne, int ligne, String contenu) {
+        return "";
+    }
+
+    public String getValue(int ligne, int col) {
+        return "";
+    }
+
+    String getFormula(int ligne, int col) {
+        return "";
+    }
+
+    String set(int ligne, int col, String newVal) {
+        return "";
+    }
+
+    void removeAllData() {
+
+    }
+}
